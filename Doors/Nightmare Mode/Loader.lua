0258be35d2d7a9f81f9a1b2ffcbf88930c142f85
Matei <90961987+MateiDaBest@users.noreply.github.com>
2023-01-29 20:29:32 +0000

    Update Loader.lua

--- Doors/Nightmare Mode/Loader.lua
+++ Doors/Nightmare Mode/Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/MateiDaBest/Utilities/main/Doors/Nightmare%20Mode/Main', true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/MateiDaBest/Utilities/main/Doors/Nightmare%20Mode/Main.lua', true))()
